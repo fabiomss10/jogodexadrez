670a2a00b6a88be56c5ee06390f81cc561b6179d
Fabio Matheus <fabiomatheussantos5036@gmail.com>
2024-03-14 13:52:31 -0300

    Primeira classe: Posicao

--- src/app/Programa.java
+++ src/app/Programa.java
@@ -1,10 +1,24 @@
 package app;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
